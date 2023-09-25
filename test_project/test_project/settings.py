8a390ab27e7daaf66e8e051183fcd037220a764f
Donnavan Stadd <donnystaddon@gmail.com>
2023-09-25 09:53:13 +0000

    Allowed host push

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53+xvsb(_+tizw-t(f17q^7dzer97v@2z512-7-#%wp9$xe0k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-donnavanstadd-fsfdjango-oo8k2bku93p.ws-eu104.gitpod.io']
 
 
 # Application definition
