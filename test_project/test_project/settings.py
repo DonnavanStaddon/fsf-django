beda061c3a2e290c5954e603d3f13e04181658fa
Donnavan Stadd <donnystaddon@gmail.com>
2023-09-26 18:10:47 +0000

    Created super user login to db

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53+xvsb(_+tizw-t(f17q^7dzer97v@2z512-7-#%wp9$xe0k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-fsfdjango-a6ad0d9zpl7.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-fsfdjango-1dp934tn1bs.ws-eu105.gitpod.io']
 
 
 # Application definition
