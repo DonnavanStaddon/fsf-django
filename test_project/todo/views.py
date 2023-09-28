1d7c7ed5620c8eeab0ebb8a4e9157f80acd189d0
Donnavan Stadd <donnystaddon@gmail.com>
2023-09-28 07:49:51 +0000

    add to db form

--- test_project/todo/views.py
+++ test_project/todo/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Item
 
-# Create your views here.
+# Get to do list from db.
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+    # add item to db.
+def add_item(request):
+    return render(request, 'todo/add_item.html')
