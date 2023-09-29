4ed0baffcd8b97f9ebc10d1a3c7d0843b14124f2
Donnavan Stadd <donnystaddon@gmail.com>
2023-09-29 07:25:44 +0000

    Forms

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53+xvsb(_+tizw-t(f17q^7dzer97v@2z512-7-#%wp9$xe0k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-donnavanstadd-fsfdjango-30gt5twa9ea.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-donnavanstadd-fsfdjango-082c8oa66pw.ws-eu105.gitpod.io']
 
 
 # Application definition
